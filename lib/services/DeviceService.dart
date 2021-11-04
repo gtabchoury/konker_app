9c8e5a2c299941a9720f254c3b67fea5279312db
Gabriel Tabchoury <g.tabchoury@gmail.com>
2021-11-04 12:14:57 -0300

    ajustando tabela

--- lib/services/DeviceService.dart
+++ lib/services/DeviceService.dart
@@ -4,34 +4,29 @@ import 'package:konker_app/models/Device.dart';
 import 'package:konker_app/utils/Constants.dart';
 
 class DeviceService {
-
-  static Future<List<Device>> getAll(String applicationName, String authToken) async{
-    var headers = {
-      'Authorization': 'Bearer $authToken'
-    };
+  static Future<List<Device>> getAll(
+      String applicationName, String authToken) async {
+    var headers = {'Authorization': 'Bearer $authToken'};
 
     http.Response response = await http.get(
         Uri.parse('${Constants.BASE_API_URL}/$applicationName/devices/'),
-        headers: headers
-    );
+        headers: headers);
 
     if (response.statusCode == 200) {
       Iterable l = json.decode(response.body)['result'];
-      return List<Device>.from(l.map((model)=> Device.fromJson(model)));
+      return List<Device>.from(l.map((model) => Device.fromJson(model)));
     } else {
       throw Exception("Erro ao obter dispositivos");
     }
   }
 
-  static Future<Device> getByGuid(String guid, String applicationName, String authToken) async{
-    var headers = {
-      'Authorization': 'Bearer $authToken'
-    };
+  static Future<Device> getByGuid(
+      String guid, String applicationName, String authToken) async {
+    var headers = {'Authorization': 'Bearer $authToken'};
 
     http.Response response = await http.get(
         Uri.parse('${Constants.BASE_API_URL}/$applicationName/devices/$guid/'),
-        headers: headers
-    );
+        headers: headers);
 
     if (response.statusCode == 200) {
       return Device.fromJson(json.decode(response.body)['result']);
@@ -40,7 +35,8 @@ class DeviceService {
     }
   }
 
-  static Future<Device> create(Device device, String applicationName, String authToken) async{
+  static Future<Device> create(
+      Device device, String applicationName, String authToken) async {
     var headers = {
       'Authorization': 'Bearer $authToken',
       'Content-Type': 'application/json'
@@ -51,20 +47,20 @@ class DeviceService {
     http.Response response = await http.post(
         Uri.parse('${Constants.BASE_API_URL}/$applicationName/devices/'),
         headers: headers,
-        body: jsonBody
-    );
+        body: jsonBody);
 
-    print("BODY: "+response.body);
-    print("STATUS: "+response.statusCode.toString());
+    print("BODY: " + response.body);
+    print("STATUS: " + response.statusCode.toString());
 
     if (response.statusCode == 201 || response.statusCode == 200) {
       return Device.fromJson(json.decode(response.body)['result']);
     } else {
-      throw Exception("Erro ao cadastrar dispositivo: "+response.body);
+      throw Exception("Erro ao cadastrar dispositivo: " + response.body);
     }
   }
 
-  static Future<bool> edit(Device device, String guid, String applicationName, String authToken) async{
+  static Future<bool> edit(Device device, String guid, String applicationName,
+      String authToken) async {
     var headers = {
       'Authorization': 'Bearer $authToken',
       'Content-Type': 'application/json'
@@ -74,30 +70,48 @@ class DeviceService {
     http.Response response = await http.put(
         Uri.parse('${Constants.BASE_API_URL}/$applicationName/devices/$guid/'),
         headers: headers,
-        body: jsonBody
-    );
+        body: jsonBody);
 
     if (response.statusCode == 201 || response.statusCode == 200) {
       return true;
     } else {
-      throw Exception("Erro ao atualizar dispositivo: "+response.body);
+      throw Exception("Erro ao atualizar dispositivo: " + response.body);
     }
   }
 
-  static Future<bool> delete(String deviceGuid, String applicationName, String authToken) async{
+  static Future<bool> delete(
+      String deviceGuid, String applicationName, String authToken) async {
     var headers = {
       'Authorization': 'Bearer $authToken',
     };
 
     http.Response response = await http.delete(
-        Uri.parse('${Constants.BASE_API_URL}/$applicationName/devices/$deviceGuid'),
-        headers: headers,
+      Uri.parse(
+          '${Constants.BASE_API_URL}/$applicationName/devices/$deviceGuid'),
+      headers: headers,
     );
 
     if (response.statusCode == 200 || response.statusCode == 204) {
       return true;
     } else {
-      throw Exception("Erro ao remover dispositivo: "+response.body);
+      throw Exception("Erro ao remover dispositivo: " + response.body);
+    }
+  }
+
+  static Future<List<dynamic>> getEvents(
+      String applicationName, String authToken, String device) async {
+    var headers = {'Authorization': 'Bearer $authToken'};
+
+    http.Response response = await http.get(
+        Uri.parse(
+            '${Constants.BASE_API_URL}/$applicationName/incomingEvents?q=device:$device'),
+        headers: headers);
+
+    if (response.statusCode == 200) {
+      Iterable l = json.decode(response.body)['result'];
+      return List<dynamic>.from(l.map((model) => model["payload"]));
+    } else {
+      throw Exception("Erro ao obter eventos do dispositivo");
     }
   }
 }
--- lib/services/RestDestinationService.dart
+++ lib/services/RestDestinationService.dart
@@ -1,38 +1,35 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'package:http/http.dart' as http;
 import 'package:konker_app/models/RestDestination.dart';
 import 'package:konker_app/utils/Constants.dart';
 
 class RestDestinationService {
-
-  static Future<List<RestDestination>> getAll(String applicationName, String authToken) async{
-    var headers = {
-      'Authorization': 'Bearer $authToken'
-    };
+  static Future<List<RestDestination>> getAll(
+      String applicationName, String authToken) async {
+    var headers = {'Authorization': 'Bearer $authToken'};
 
     http.Response response = await http.get(
-        Uri.parse('${Constants.BASE_API_URL}/$applicationName/restDestinations/'),
-        headers: headers
-    );
+        Uri.parse(
+            '${Constants.BASE_API_URL}/$applicationName/restDestinations/'),
+        headers: headers);
 
     if (response.statusCode == 200) {
       Iterable l = json.decode(response.body)['result'];
-      return List<RestDestination>.from(l.map((model)=> RestDestination.fromJson(model)));
+      return List<RestDestination>.from(
+          l.map((model) => RestDestination.fromJson(model)));
     } else {
       throw Exception("Erro ao obter destinos Rest");
     }
   }
 
-  static Future<RestDestination> getByGuid(String guid, String applicationName, String authToken) async{
-    var headers = {
-      'Authorization': 'Bearer $authToken'
-    };
+  static Future<RestDestination> getByGuid(
+      String guid, String applicationName, String authToken) async {
+    var headers = {'Authorization': 'Bearer $authToken'};
 
     http.Response response = await http.get(
-        Uri.parse('${Constants.BASE_API_URL}/$applicationName/restDestinations/$guid/'),
-        headers: headers
-    );
+        Uri.parse(
+            '${Constants.BASE_API_URL}/$applicationName/restDestinations/$guid/'),
+        headers: headers);
 
     if (response.statusCode == 200) {
       return RestDestination.fromJson(json.decode(response.body)['result']);
@@ -41,7 +38,8 @@ class RestDestinationService {
     }
   }
 
-  static Future<RestDestination> create(RestDestination restDestination, String applicationName, String authToken) async{
+  static Future<RestDestination> create(RestDestination restDestination,
+      String applicationName, String authToken) async {
     var headers = {
       'Authorization': 'Bearer $authToken',
       'Content-Type': 'application/json'
@@ -52,19 +50,20 @@ class RestDestinationService {
     print(jsonBody);
 
     http.Response response = await http.post(
-        Uri.parse('${Constants.BASE_API_URL}/$applicationName/restDestinations/'),
+        Uri.parse(
+            '${Constants.BASE_API_URL}/$applicationName/restDestinations/'),
         headers: headers,
-        body: jsonBody
-    );
+        body: jsonBody);
 
     if (response.statusCode == 201 || response.statusCode == 200) {
       return RestDestination.fromJson(json.decode(response.body)['result']);
     } else {
-      throw Exception("Erro ao cadastrar destino Rest: "+response.body);
+      throw Exception("Erro ao cadastrar destino Rest: " + response.body);
     }
   }
 
-  static Future<bool> edit(RestDestination restDestination, String guid, String applicationName, String authToken) async{
+  static Future<bool> edit(RestDestination restDestination, String guid,
+      String applicationName, String authToken) async {
     var headers = {
       'Authorization': 'Bearer $authToken',
       'Content-Type': 'application/json'
@@ -72,33 +71,34 @@ class RestDestinationService {
 
     var jsonBody = jsonEncode(restDestination.toJson());
     http.Response response = await http.put(
-        Uri.parse('${Constants.BASE_API_URL}/$applicationName/restDestinations/$guid/'),
+        Uri.parse(
+            '${Constants.BASE_API_URL}/$applicationName/restDestinations/$guid/'),
         headers: headers,
-        body: jsonBody
-    );
+        body: jsonBody);
 
     if (response.statusCode == 201 || response.statusCode == 200) {
       return true;
     } else {
-      throw Exception("Erro ao atualizar destino Rest: "+response.body);
+      throw Exception("Erro ao atualizar destino Rest: " + response.body);
     }
   }
 
-  static Future<bool> delete(String restDestinationGuid, String applicationName, String authToken) async{
+  static Future<bool> delete(String restDestinationGuid, String applicationName,
+      String authToken) async {
     var headers = {
       'Authorization': 'Bearer $authToken',
     };
 
     http.Response response = await http.delete(
-      Uri.parse('${Constants.BASE_API_URL}/$applicationName/restDestinations/$restDestinationGuid'),
+      Uri.parse(
+          '${Constants.BASE_API_URL}/$applicationName/restDestinations/$restDestinationGuid'),
       headers: headers,
     );
 
     if (response.statusCode == 200 || response.statusCode == 204) {
       return true;
     } else {
-      throw Exception("Erro ao remover destino Rest: "+response.body);
+      throw Exception("Erro ao remover destino Rest: " + response.body);
     }
   }
-
 }
